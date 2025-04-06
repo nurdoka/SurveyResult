46017287cf7bb1528492f3207dd5f55ec5706df0
Nurdoka <nurdoolot93@gmail.com>
2025-04-06 22:51:48 +0600

    QuestionCounter was modified

--- src/utils/questionCounter.jsx
+++ src/utils/questionCounter.jsx
@@ -1,19 +1,29 @@
 export const countQuestionIds = (responses) => {
-    const groupedResponses = [];
-
+    let groupedResponses = [];
     for (let i = 0; i < responses.length; i++) {
-        const questionId = responses[i].question;
-        const selectedOption = responses[i].selected_option;
-
-        const existingGroup = groupedResponses.find(item => item.question === questionId);
+        const {
+            question: questionId,
+            selected_option: selectedOption,
+            custom_answer: customAnswer,
+            multiple_selected_options: multipleSelected
+        } = responses[i];
+        let existingGroup = groupedResponses.find(item => item.question === questionId);
 
         if (!existingGroup) {
-            groupedResponses.push({ question: questionId, answers: [selectedOption] });
-        } else if(selectedOption){
-            existingGroup.answers.push(selectedOption);
+            groupedResponses.push({
+                question: questionId,
+                selectedOptions: [
+                    ...(selectedOption !== null ? [selectedOption] : []),
+                    ...(Array.isArray(multipleSelected) ? multipleSelected : [])
+                ],
+                customAnswers: customAnswer ? [customAnswer] : []
+            });
+        } else {
+            if (selectedOption !== null) existingGroup.selectedOptions.push(selectedOption);
+            if (Array.isArray(multipleSelected)) existingGroup.selectedOptions.push(...multipleSelected);
+            if (customAnswer) existingGroup.customAnswers.push(customAnswer);
         }
     }
 
-    // console.log("groupedResponses", groupedResponses);
     return groupedResponses;
 };
