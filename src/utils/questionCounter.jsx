35e62aa52eec2bfd852f28da85f92236b20ef5c3
Nurdoka <nurdoolot93@gmail.com>
2025-04-06 19:50:43 +0600

    QuestionCounter was modified

--- src/utils/questionCounter.jsx
+++ src/utils/questionCounter.jsx
@@ -14,6 +14,6 @@ export const countQuestionIds = (responses) => {
         }
     }
 
-    console.log("groupedResponses", groupedResponses);
+    // console.log("groupedResponses", groupedResponses);
     return groupedResponses;
 };
